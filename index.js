f9c05a497b696019a2b777e43c017d17e337f631
calvin echols <calvin.echols@gmail.com>
2017-11-09 22:12:00 -0500

    getting close to working.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Crypto = require('crypto');
-// const uuid = require('uuid/v4')
+const uuidv4 = require('uuid/v4')
 
 const defaultOptions = {
     header: {
         typ: 'JWT',
-        alg: 'HS256'
+        alg: 'HS512'
     },
     payload: {
         iss: undefined,
@@ -19,26 +19,66 @@ class JWT {
     constructor (config) {
         self.header = Object.assign({}, config.header, defaultOptions.header);
         self.payload = Object.assign({}, config.payload, defaultOptions.payload);
+        self.signature = null;
         self.secret = config.secret
         self.token = null;
         
     }
 
-    sign (alg, secret) {
-        let hmac = null;
-        if (self.header.alg === 'HS256') {
-            hmac = Crypto.createHmac('sha256', secret);
-        } else if (self.header.alg === 'HS512') {
-            hmac = Crypto.createHmac('sha512', secret);
-        }
-        const headerString = new Buffer(JSON.stringify(self.header)).toString('base64');
-        const payloadString = new Buffer(JSON.stringify(self.payload)).toString('base64');
-
-
+    sign (header, payload, alg, secret) {       
+        const headerString = _base64EncodeObject(header);
+        const payloadString = _base64EncodeObject(payload);
+        const signature = _getSignature(headerString, payloadString, header.alg, secret);//hmac.digest('base64');
+        self.token = `${self.headerString}.${self.payloadString}.${self.signature}`;
+    }
+    
+    static verify(token, secret) {
+        let parts = token.split('.');
+        let signature = parts[2];
+        const header = _base64StringToUTF8(parts[0]);
+        const incommingSignature = _getSignature(parts[0], parts[1], header.alg, secret);
+        return signature === incommingSignature;
     }
 
-    static verify(token, secret) {
+    static generateRandomSecret (numRuns = 5) {
+        let secret = '';
+        for(let i = 0; i < numRuns; i++){
+            secret += uuidv4();
+        }
+        return secret.replace('-', '');
+    }
 
+    _getHmac (alg, secret) {
+        let hmac = null;
+        if (alg === 'HS256') {
+            hmac = Crypto.createHmac('sha256', secret);
+        } else if (alg === 'HS512') {
+            hmac = Crypto.createHmac('sha512', secret);
+        } else {
+            hmac = Crypto.createHmac(alg, secret)
+        }
+        if(hmac === null) {
+            throw `${alg} is not a supported algorythm!`;
+        }
+        return hmac;
+    }
+
+    _getSignature (base64Header, base64Payload, alg, secret) {
+        let hmac = _getHmac(header.alg, secret); 
+        hmac.update(`${headerString}.${payloadString}`);
+        return hmac.digest('base64');
+    }
+
+    _base64EncodeObject (obj) {
+        return _base64EncodeString(JSON.stringify(obj));
+    }
+
+    _base64EncodeString (str) {
+        return new Buffer(str, 'utf8').toString('base64');
+    }
+
+    _base64StringToUTF8 (str) {
+        return new Buffer(str, 'base64').toString('utf8');
     }
 
 
