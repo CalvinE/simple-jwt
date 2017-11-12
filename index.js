e9a0345602d2ca6aaadd9a3d906a3936e4ed60e4
calvin echols <calvin.echols@gmail.com>
2017-11-11 23:22:49 -0500

    working!

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 const Crypto = require('crypto');
 const uuidv4 = require('uuid/v4')
 
-const defaultOptions = {
-    header: {
-        typ: 'JWT',
-        alg: 'HS512'
-    },
-    payload: {
-        iss: undefined,
-        exp: undefined,
-        nbf: undefined,
-        sub: undefined,
-        aud: undefined
-    }
-};
+// This is just for reference.
+// const defaultOptions = {
+//     header: {
+//         typ: 'JWT',
+//         alg: 'HS512'
+//     },
+//     payload: {
+//         iss: undefined,
+//         exp: undefined,
+//         nbf: undefined,
+//         sub: undefined,
+//         aud: undefined
+//     }
+// };
 
 class JWT {
-    constructor (config) {
-        self.header = Object.assign({}, config.header, defaultOptions.header);
-        self.payload = Object.assign({}, config.payload, defaultOptions.payload);
-        self.signature = null;
-        self.secret = config.secret
-        self.token = null;
-        
+    constructor () {
     }
 
-    sign (header, payload, alg, secret) {       
-        const headerString = _base64EncodeObject(header);
-        const payloadString = _base64EncodeObject(payload);
-        const signature = _getSignature(headerString, payloadString, header.alg, secret);//hmac.digest('base64');
-        self.token = `${self.headerString}.${self.payloadString}.${self.signature}`;
+    static getToken (header, payload, secret, base64EncodeSecret = false) {       
+        const headerString = this._base64EncodeObject(header);
+        const payloadString = this._base64EncodeObject(payload);
+        if(base64EncodeSecret === true) {
+            secret = this._base64EncodeString(secret);
+        }
+        const signature = this._getSignature(headerString, payloadString, header.alg, secret);//hmac.digest('base64');
+        return `${headerString}.${payloadString}.${signature}`;
     }
     
-    static verify(token, secret) {
+    static verifyToken(token, secret, base64EncodeSecret = false) {
         let parts = token.split('.');
         let signature = parts[2];
-        const header = _base64StringToUTF8(parts[0]);
-        const incommingSignature = _getSignature(parts[0], parts[1], header.alg, secret);
+        const header = this._base64StringToUTF8(parts[0]);
+        if(base64EncodeSecret === true) {
+            secret = this._base64EncodeString(secret);
+        }
+        const incommingSignature = this._getSignature(parts[0], parts[1], header.alg, secret);
         return signature === incommingSignature;
     }
 
@@ -45,10 +46,10 @@ class JWT {
         for(let i = 0; i < numRuns; i++){
             secret += uuidv4();
         }
-        return secret.replace('-', '');
+        return secret.replace(/-/g, '');
     }
 
-    _getHmac (alg, secret) {
+    static _getHmac (alg, secret) {
         let hmac = null;
         if (alg === 'HS256') {
             hmac = Crypto.createHmac('sha256', secret);
@@ -63,23 +64,25 @@ class JWT {
         return hmac;
     }
 
-    _getSignature (base64Header, base64Payload, alg, secret) {
-        let hmac = _getHmac(header.alg, secret); 
-        hmac.update(`${headerString}.${payloadString}`);
+    static _getSignature (base64Header, base64Payload, alg, secret) {
+        const header = JSON.parse(this._base64StringToUTF8(base64Header));
+        let hmac = this._getHmac(header.alg, secret); 
+        hmac.update(`${base64Header}.${base64Payload}`);
         return hmac.digest('base64');
     }
 
-    _base64EncodeObject (obj) {
-        return _base64EncodeString(JSON.stringify(obj));
+    static _base64EncodeObject (obj) {
+        return this._base64EncodeString(JSON.stringify(obj));
     }
 
-    _base64EncodeString (str) {
+    static _base64EncodeString (str) {
         return new Buffer(str, 'utf8').toString('base64');
     }
 
-    _base64StringToUTF8 (str) {
+    static _base64StringToUTF8 (str) {
         return new Buffer(str, 'base64').toString('utf8');
     }
 
-
 }
+
+module.exports = JWT;
